1338f5c2e798f3344a44c2fe8e4b07f7db235b85
申誉 <sparkshen0807@gmail.com>
2021-11-03 02:13:31 -0700

    Add 0026.cpp

---
+++ LeetCode/0026.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /*
+    Use a variable n to store the last index of the transformed array. 
+    Traverse nums. If a new number is found, insert it at nums[n+1]. 
+    Time complexity: O(n), Space complexity: O(1). 
+    */
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) 
+            return 0; 
+
+        int n = 0;  
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[n+1] = nums[i]; 
+                n += 1;
+            }
+        }
+        return n + 1; 
+    }
+};
