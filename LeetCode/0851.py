a50c37f22136f2607cfba06fccb5c19025cf9a0e
Yu Shen <y.shen@ucla.edu>
2021-12-15 02:28:19 -0500

    Add 0851.py

---
+++ LeetCode/0851.py
@@ -0,0 +1,27 @@
+class Solution:
+    '''
+    First, build a tree from richer, such that each node is richer than its parent. 
+    Second, calculate the answer of each node from the answers of its children. 
+    Time complexity: O(# of nodes + # of edges), Space complexity: O(# of nodes + # of edges). 
+    '''
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n = len(quiet)
+        node2Children = {node: [] for node in range(n)}
+        for a, b in richer:
+            node2Children[b].append(a)
+
+        def calAnswer(node, answer):
+            if answer[node] != None:
+                return answer
+            curAns = node
+            for child in node2Children[node]:
+                if answer[child] == None:
+                    answer = calAnswer(child, answer)
+                if quiet[answer[child]] < quiet[curAns]:
+                    curAns = answer[child]
+            answer[node] = curAns
+            return answer
+        answer = [None] * n
+        for node in range(n):
+            answer = calAnswer(node, answer)
+        return answer
