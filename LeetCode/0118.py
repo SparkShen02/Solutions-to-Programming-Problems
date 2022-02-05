7bc16b3f79ff748b273d3973d80fd64541b1804f
Yu Shen <y.shen@ucla.edu>
2022-02-05 13:24:36 -0800

    Update 0118.py

--- LeetCode/0118.py
+++ LeetCode/0118.py
@@ -6,12 +6,10 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         ans = [[-1]*(i+1) for i in range(numRows)]
         for i in range(numRows):
-            n = len(ans[i])
+            n = i + 1
             for j in range(n):
                 if j == 0 or j == n-1:
                     ans[i][j] = 1
                 else:
                     ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
         return ans
-            
-        
