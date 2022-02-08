f319e6d78431afe186e4c032c18fbf61e7c13163
Yu Shen <y.shen@ucla.edu>
2022-02-07 18:54:10 -0800

    Add 0377.py

---
+++ LeetCode/0377.py
@@ -0,0 +1,15 @@
+class Solution:
+    '''
+    Dynamic programming. 
+    dp[n] = the number of possible sequences that add up to n
+    dp[n] = sum(dp[n-num] for num in nums)
+    Time complexity: O(n), Space complexity: O(n). 
+    '''
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0 for n in range(target+1)]
+        dp[0] = 1
+        for n in range(1, target+1):
+            for num in nums:
+                if n-num >= 0:
+                    dp[n] += dp[n-num]
+        return dp[target]
