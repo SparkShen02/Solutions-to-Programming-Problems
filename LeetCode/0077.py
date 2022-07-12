6eb389055932ad0365f47d0f0bd98daa8f6a98a1
Yu Shen <y.shen@ucla.edu>
2022-07-11 23:02:33 -0700

    Update 0077.py

--- LeetCode/0077.py
+++ LeetCode/0077.py
@@ -7,18 +7,14 @@ class Solution:
         ans = []
         curAns = [0] * k
 
-        def backtrack(i):
-            if i == k:
+        def backtrack(ansLen, lastNum):
+            if ansLen == k:
                 ans.append(curAns[:])
                 return
 
-            if i == 0:
-                lastNum = 0
-            else:
-                lastNum = curAns[i-1]
             for candidate in range(lastNum+1, n+1):
-                curAns[i] = candidate
-                backtrack(i+1)
+                curAns[ansLen] = candidate
+                backtrack(ansLen+1, candidate)
         
-        backtrack(0)
+        backtrack(0, 0)
         return ans
